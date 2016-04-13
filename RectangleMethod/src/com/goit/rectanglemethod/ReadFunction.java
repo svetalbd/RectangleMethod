14d1670d1ccb72287eec2697353f54b86f4d13c8
mykhailenko.s@ukr.net <mykhailenko.s@ukr.net>
2016-04-13 15:46:12 +0300

    calculate without exceptions

--- RectangleMethod/src/com/goit/rectanglemethod/ReadFunction.java
+++ RectangleMethod/src/com/goit/rectanglemethod/ReadFunction.java
@@ -1,6 +1,6 @@
 package com.goit.rectanglemethod;
 
-import java.util.Scanner;
+import java.util.*;
 
 /**
  * Created by Сергей on 12.04.2016.
@@ -8,5 +8,159 @@ import java.util.Scanner;
 public class ReadFunction {
 
 
+    public static final Map<String, Integer> OPERATORS;
+    static {
+        OPERATORS = new HashMap<String, Integer>();
+        OPERATORS.put("^", 1);
+        OPERATORS.put("*", 2);
+        OPERATORS.put("/", 2);
+        OPERATORS.put("sin", 2);
+        OPERATORS.put("cos", 2);
+        OPERATORS.put("exp", 2);
+        OPERATORS.put("tg", 2);
+        OPERATORS.put("arctan", 2);
+        OPERATORS.put("-", 3);
+        OPERATORS.put("+", 3);
+        OPERATORS.put("(", 4);
+        OPERATORS.put(")", 4);
+    }
+
+    private static String preChange (String newFunction){
+        newFunction = newFunction.replaceAll(" ", "");
+        newFunction = newFunction.replaceAll(",", ".");
+        return newFunction;
+    }
+
+
+    public static String conversionToRPN(String newFunction, Map<String, Integer> operations) {
+        String leftBracket = "(";
+        String rightBracket = ")";
+        int index = 0;
+        boolean endOperation = false;
+
+        newFunction = preChange(newFunction);
+        List<String> listOfConversationsToRPN = new ArrayList<String>();
+        Stack<String> stackOfOperations = new Stack<String>();
+        Set<String> allOperations = new HashSet<String>(operations.keySet());
+
+
+        while (!endOperation) {
+            int nextOperationIndex = newFunction.length();
+            String nextOperation = "";
+            for (String operation : allOperations) {
+                int i = newFunction.indexOf(operation, index);
+                if (i >= 0 && i < nextOperationIndex) {
+                    nextOperation = operation;
+                    nextOperationIndex = i;
+                }
+            }
+            if (nextOperationIndex == newFunction.length()) {
+                endOperation = true;
+            } else {
+                if (index != nextOperationIndex) {
+                    listOfConversationsToRPN.add(newFunction.substring(index, nextOperationIndex));
+                }
+                if (nextOperation.equals(leftBracket)) {
+                    stackOfOperations.push(nextOperation);
+                } else if (nextOperation.equals(rightBracket)) {
+                    while (!stackOfOperations.peek().equals(leftBracket)) {
+                        listOfConversationsToRPN.add(stackOfOperations.pop());
+                        if (stackOfOperations.empty()) {
+                            System.out.println("ERROR: Empty brackets.");
+                            break;
+                        }
+                    }
+                    stackOfOperations.pop();
+                } else {
+                    while (!stackOfOperations.empty() && !stackOfOperations.peek().equals(leftBracket) &&
+                            (operations.get(nextOperation) >= operations.get(stackOfOperations.peek()))) {
+                        listOfConversationsToRPN.add(stackOfOperations.pop());
+                    }
+                    stackOfOperations.push(nextOperation);
+                }
+                index = nextOperationIndex + nextOperation.length();
+            }
+        }
+
+        if (index != newFunction.length()) {
+            listOfConversationsToRPN.add(newFunction.substring(index));
+        }
+        while (!stackOfOperations.empty()) {
+            listOfConversationsToRPN.add(stackOfOperations.pop());
+        }
+        StringBuffer rpn = new StringBuffer();
+        if (!listOfConversationsToRPN.isEmpty()) {
+            rpn.append(listOfConversationsToRPN.remove(0));
+        }
+        while (!listOfConversationsToRPN.isEmpty()) {
+            rpn.append(" ").append(listOfConversationsToRPN.remove(0));
+        }
+
+        return rpn.toString();
+    }
+
+
+    public static Double calculateIntegral(String expression, Double x) {
+        String rpn = conversionToRPN(expression, OPERATORS);
+        String[] delims = rpn.split(" ");
+        Stack<Double> result = new Stack<Double>();
+
+        for (int i = 0; i < delims.length; i++){
+            String e = delims[i];
+            if (!OPERATORS.keySet().contains(e)) {
+                if (e.equals("pi") || e.equals("Pi") || e.equals("PI")) {
+                    result.push(Math.PI);
+                } else if (e.equals("e") || e.equals("E")) {
+                    result.push(Math.E);
+                } else  if (delims[i].equals("x") || delims[i].equals("y") || delims[i].equals("z")){
+                    result.push(x);
+                } else {
+                    result.push(new Double(e));
+                }
+            } else {
+                Double op2 = result.pop();
+                Double op1 = result.empty() ? 0d : result.pop();
+                switch (e) {
+                    case "*" : 	result.push(op1 * op2);
+                        break;
+                    case "/" : 	result.push(op1 / op2);
+                        break;
+                    case "+" : 	result.push(op1 + op2);
+                        break;
+                    case "-" : 	result.push(op1 - op2);
+                        break;
+                    case "^" : 	result.push(Math.pow(op1, op2));
+                        break;
+                    case "sin" : 	if (op1 != 0) {
+                        result.push(op1);
+                    }
+                        result.push(Math.sin(op2));
+                        break;
+                    case "cos" : 	if (op1 != 0) {
+                        result.push(op1);
+                    }
+                        result.push(Math.cos(op2));
+                        break;
+                    case "tg" : 	if (op1 != 0) {
+                        result.push(op1);
+                    }
+                        result.push(Math.tan(op2));
+                        break;
+                    case "arctan" : 	if (op1 != 0) {
+                        result.push(op1);
+                    }
+                        result.push(Math.atan(op2));
+                        break;
+                    case "exp" : 	if (op1 != 0) {
+                        result.push(op1);
+                    }
+                        result.push(Math.exp(op2));
+                        break;
+                    default: 	System.out.println("Error!");
+                }
+            }
+        }
+        return (double) Math.round(result.pop());
+    }
 
 }
--- RectangleMethod/src/com/goit/rectanglemethod/Runner.java
+++ RectangleMethod/src/com/goit/rectanglemethod/Runner.java
@@ -7,15 +7,42 @@ import java.util.Scanner;
  */
 public class Runner {
     public static void main (String[] args){
-        System.out.print("Input your formula:");
+        System.out.println("Input your integral. Variable must be named \"y\":");
         Scanner sc = new Scanner(System.in);
+        String expression = sc.nextLine();
+        ReadFunction readFunction = new ReadFunction();
 
-        String phrase = sc.next();
-        String delims = "[+*/^()-]+";
-        String[] tokens = phrase.split(delims);
+        readFunction.calculateIntegral(expression, 2d);
 
-        for (String each: tokens) {
-            System.out.println(each);
+        System.out.println("Input lower borders");
+        /*Можно сделать класс для границ, чтобы вы все к нему обращались.*/
+        Double lowerBorder = sc.nextDouble();
+
+
+        System.out.println("Input upper borders");
+        /*смотрите комментарий выше*/
+        Double upperBorder = sc.nextDouble();
+
+
+        System.out.println("Input number of steps");
+        /*смотрите комментарий выше*/
+        Integer countOfSteps = sc.nextInt();
+
+        System.out.println("1 - The method of LEFT corner rectangles\n " +
+                "2 - The method of CENTER corner rectangles\n " +
+                "3 - The method of RIGHT corner rectangles\n");
+        Integer choice = sc.nextInt();
+        switch (choice){
+            case 1://метод левых прямоугольников
+                break;
+            case 2: //метод центральных прямоугольников
+                break;
+            case 3: //метод правых прямоугольников
+                break;
+            default:
+                System.out.println("Make good choice");
+                break;
         }
+
     }
 }
