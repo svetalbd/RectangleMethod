5d47b303aa0b864ed1ae213cc3c34a680153e8c4
mykhailenko.s@ukr.net <mykhailenko.s@ukr.net>
2016-04-14 12:55:04 +0300

    added exceptions

--- RectangleMethod/src/com/goit/rectanglemethod/LowerUpperBorder.java
+++ RectangleMethod/src/com/goit/rectanglemethod/LowerUpperBorder.java
@@ -4,4 +4,33 @@ package com.goit.rectanglemethod;
  * Created by Сергей on 14.04.2016.
  */
 public class LowerUpperBorder {
+    private double lowerBorder;
+    private double upperBorder;
+    private double countOfSteps;
+
+    public double getLowerBorder() {
+        return lowerBorder;
+    }
+
+    public void setLowerBorder(double lowerBorder) {
+        this.lowerBorder = lowerBorder;
+    }
+
+    public double getUpperBorder() {
+        return upperBorder;
+    }
+
+    public void setUpperBorder(double upperBorder) {
+        this.upperBorder = upperBorder;
+    }
+
+    public double getCountOfSteps() {
+        return countOfSteps;
+    }
+
+    public void setCountOfSteps(double countOfSteps) {
+        this.countOfSteps = countOfSteps;
+    }
+
+
 }
--- RectangleMethod/src/com/goit/rectanglemethod/Runner.java
+++ RectangleMethod/src/com/goit/rectanglemethod/Runner.java
@@ -13,6 +13,7 @@ public class Runner {
         Scanner sc = new Scanner(System.in);
         String expression = sc.nextLine();
         ReadFunction readFunction = new ReadFunction();
+        LowerUpperBorder lowerUpperBorder = new LowerUpperBorder();
 
         System.out.print(readFunction.calculateIntegral(expression, 2d));
 
@@ -20,6 +21,7 @@ public class Runner {
         /*Можно сделать класс для границ, чтобы вы все к нему обращались.*/
         try {
             Double lowerBorder = sc.nextDouble();
+            lowerUpperBorder.setLowerBorder(lowerBorder);
         } catch (InputMismatchException e) {
             System.out.println("ERROR: it must be a number.");
         }
@@ -29,6 +31,7 @@ public class Runner {
         /*смотрите комментарий выше*/
         try {
             Double upperBorder = sc.nextDouble();
+            lowerUpperBorder.setUpperBorder(upperBorder);
         } catch (InputMismatchException e) {
             System.out.println("ERROR: it must be a number.");
         }
@@ -36,7 +39,12 @@ public class Runner {
 
         System.out.print("Input count of steps, please: ");
         /*смотрите комментарий выше*/
+        try {
         Integer countOfSteps = sc.nextInt();
+        lowerUpperBorder.setCountOfSteps(countOfSteps);
+        } catch (InputMismatchException e) {
+            System.out.println("ERROR: it must be an integer number.");
+        }
 
         System.out.println("1 - The method of LEFT corner rectangles\n " +
                 "2 - The method of CENTER corner rectangles\n " +
